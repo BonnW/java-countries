4b62ffb86630e09d938c056baa04163934b60043
bonnw <pwonghansa@gmail.com>
2019-02-11 13:07:47 -0600

    initialized project with intelliJ

---
+++ src/main/java/com/burahan/countrydemo/CountryDemoApplication.java
@@ -0,0 +1,16 @@
+package com.burahan.countrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CountryDemoApplication.class, args);
+    }
+
+}
+
