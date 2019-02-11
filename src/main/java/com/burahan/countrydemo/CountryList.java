d136a39548813c679864bbd8f1a83bbc71da1c34
bonnw <pwonghansa@gmail.com>
2019-02-11 15:53:22 -0600

    created Country class object

---
+++ src/main/java/com/burahan/countrydemo/CountryList.java
@@ -0,0 +1,13 @@
+package com.burahan.countrydemo;
+
+import java.util.ArrayList;
+
+public class CountryList
+{
+    public ArrayList<Country> countryList = new ArrayList<Country>();
+
+    public CountryList()
+    {
+
+    }
+}
